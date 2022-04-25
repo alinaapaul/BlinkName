520a4bd681bd99398b511f57085f8de79266fdb8
Alina Paul <72575612+alinaapaul@users.noreply.github.com>
2022-04-25 22:06:34 +1000

    Add files via upload

---
+++ BlinkName.ino
@@ -0,0 +1,83 @@
+// We define MY_LED to be the LED that we want to blink. 
+//
+// In this tutorial, we're using the blue D7 LED (next to D7 on the Photon 
+// and Electron, and next to the USB connector on the Argon and Boron).
+const pin_t MY_LED = D7;
+
+// The following line is optional, but recommended in most firmware. It 
+// allows your code to run before the cloud is connected. In this case, 
+// it will begin blinking almost immediately instead of waiting until 
+// breathing cyan,
+SYSTEM_THREAD(ENABLED);
+
+// The setup() method is called once when the device boots.
+void setup() {
+  // In order to set a pin, you must tell Device OS that the pin is 
+  // an OUTPUT pin. This is often done from setup() since you only need 
+  // to do it once.
+  pinMode(MY_LED, OUTPUT);
+}
+
+// The loop() method is called frequently.
+void loop() {
+    
+    // A
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (A = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s);  // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(3s); // Leave it on for three seconds (A = Dah = 3 Units)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(3s); // Wait three seconds (Inter-character space = 3 Units)
+  
+  // L
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (L = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s); // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(3s); // Leave it on for three seconds (L = Dah = 3 Units)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s); // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (L = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s); // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (L = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(3s); // Wait three seconds (Inter-character space = 3 Units)
+  
+  // I
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (I = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s); // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (I = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(3s); // Wait three seconds (Inter-character space = 3 Units)
+  
+  // N
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(3s); // Leave it on for three seconds (N = Dah = 3 Units)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s); // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (N = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(3s); // Wait three seconds (Inter-character space = 3 Units)
+  
+  // A
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(1s); // Leave it on for one second (A = Dit = 1 Unit)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(1s);  // Wait one second (Intra-character space = 1 Unit)
+  digitalWrite(MY_LED, HIGH); // Turn on the LED
+  delay(3s); // Leave it on for three seconds (A = Dah = 3 Units)
+  digitalWrite(MY_LED, LOW); // Turn it off
+  delay(7s); // Wait seven seconds (word space = 7 Units)
+
+  // And repeat!
+}
